650f6a06512689a628eb59b9749bc6bd495ef5f8
krishna chebrolu <chebrolu.priya@gmail.com>
2026-05-31 13:34:11 -0400

    cleanup

--- app/src/main/java/com/instagram/designsystem/core/designsystem/components/button/ButtonCatalogScreen.kt
+++ app/src/main/java/com/instagram/designsystem/core/designsystem/components/button/ButtonCatalogScreen.kt
@@ -1,16 +1,9 @@
 package com.instagram.designsystem.core.designsystem.components.button
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ButtonCatalogScreen(modifier: Modifier) {
